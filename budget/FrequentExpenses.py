26f3d1182fdd1ea613acafcaa017cef469068bb5
Ike Agbai <ikesco1@gmail.com>
2022-03-18 09:26:10 +0100

    Update FrequentExpenses.py

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,12 @@
+# Import The Expense Module
+import collections
+from . import Expense
+
+# Read in the Spending data
+expenses = Expense.Expenses()
+expenses.read_expenses("data/spending_data.csv")
+spending_categories = []
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+    spending_counter = collections.Counter(spending_categories)
+    top5 = spending_counter.most_common(5)
