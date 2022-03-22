672fca82f015981e2a56f3c71ba8f7139040879d
Ike Agbai <ikesco1@gmail.com>
2022-03-22 13:21:05 +0100

    Update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -16,10 +16,10 @@ for expense in expenses.list:
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 # Convert the Dictionary to 2 Lists
-category, count = zip(*top5)
+categories, count = zip(*top5)
 
 fig, ax = plt.subplots()
-ax.bar(category, count)
+ax.bar(categories, count)
 ax.set_title("# of Purchases by Category")
 plt.show() 
         
